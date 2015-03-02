49ca5de8ca24919a5cf03bc496685e717802d585
Derek J. Augustine <augshow@gmail.com>
2015-03-02 09:45:28 -0600

    adding in some IDE helpers

--- src/Augstudios/Alerts/AlertType.php
+++ src/Augstudios/Alerts/AlertType.php
@@ -6,10 +6,36 @@ use MyCLabs\Enum\Enum;
 
 class AlertType extends Enum
 {
-    const __default = self::Info;
-
     const Info = 'info';
     const Success = 'success';
     const Danger = 'danger';
     const Warning = 'warning';
+
+    /**
+     * @return AlertType
+     */
+    public static function Info() {
+        return new AlertType(self::Info);
+    }
+
+    /**
+     * @return AlertType
+     */
+    public static function Success() {
+        return new AlertType(self::Success);
+    }
+
+    /**
+     * @return AlertType
+     */
+    public static function Danger() {
+        return new AlertType(self::Danger);
+    }
+
+    /**
+     * @return AlertType
+     */
+    public static function Warning() {
+        return new AlertType(self::Warning);
+    }
 }
